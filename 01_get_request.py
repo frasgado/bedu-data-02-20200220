666f25a0c97ba69f8b0b3704a17514b28b284270
frasgado <frasgado@gmail.com>
2021-02-20 11:11:56 -0600

    Modified.

--- 01_get_request.py
+++ 01_get_request.py
@@ -1,5 +1,13 @@
 import requests
 
-response = requests.get('https://galileoguzman.com')
+# response = requests.get('https://galileoguzman.com')
+response = requests.get('https://api.github.com/users/frasgado')
 
-print(response)
+print(response.content)
+# resultado de la peticion o de la respuesta
+# 404 contenido no disponible
+print(response.status_code)
+print('-------------------------------------')
+response = requests.get('https://avatars.githubusercontent.com/u/190476?v=4pip ')
+print(response.content)
+print(response.status_code)
