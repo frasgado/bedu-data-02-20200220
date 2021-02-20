f42dbce56d25104a55e6a4fe178295eeb76c3eb2
frasgado <frasgado@gmail.com>
2021-02-20 11:26:40 -0600

    Add file 02_github_user.py.

---
+++ 02_github_user.py
@@ -0,0 +1,32 @@
+import requests
+
+BASE_URL = 'https://api.github.com/'
+
+username = input('Give me a github username:\t')
+endpoint_url = BASE_URL + f'users/{username}'
+
+response = requests.get(endpoint_url)
+print(response.status_code) # 200 means user exists
+
+# print(response.status_code) # 200 means user exists
+user = response.json() # From json to python Dict
+
+# print(type(user))
+
+print(user['avatar_url'])
+
+
+""" import requests
+
+BASE_URL = 'https://api.github.com/users'
+
+username = input('Give me a github username:\t')
+endpoint_url = BASE_URL + f'users/{username}'
+
+print(endpoint_url)
+
+response = requests.get(endpoint_url)
+print(response.status_code)
+
+# para verlo como si fuera el navegador
+print(response.json()) """
